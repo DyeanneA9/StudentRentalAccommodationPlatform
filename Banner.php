01240527de98b67308a9e9b56e0a50f4b3e9f139
Dyeanne <dyeanneangel02@gmail.com>
2025-01-23 08:56:41 +0800

    done

--- Banner.php
+++ Banner.php
@@ -1,8 +1,8 @@
 <section class="banner">
     <div class="container-fluid px-0">
-        <div class="banner-content position-relative text-center text-white">
+        <div class="banner-content position-relative">
             <img src="logo/Header.jpeg" class="img-fluid w-100" alt="Banner Image">
-            <div class="banner-overlay position-absolute top-50 start-50 translate-middle">
+            <div class="banner-overlay">
                 <h1 class="fw-bold">FIND YOUR PERFECT STUDENT HOME</h1>
                 <p>Discover comfortable, affordable, and convenient accommodations near you.</p>
             </div>
